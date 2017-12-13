224d594fd321f3dc7b0c42a816f56f03f28ab917
boying.shi <shi.boying@gmail.com>
2017-12-13 01:08:57 -0800

    bs - add game page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,64 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+
+var currentScore = 0;
+
+$(document).ready(function () {
+  const array = $('input').toArray();
+  for (var i = 0; i < array.length; i++) {
+    if (i === 0) {
+      continue;
+    }
+    if ($('input').eq(i - 1).val() === '') {
+      $('input').eq(i).prop('disabled', true);
+    } else {
+      $('input').eq(i).prop('disabled', false);
+    }
+  }
+});
+
+$(document).on('focusin', 'input', function(){
+    $(this).data('val', $(this).val());
+});
+
+$(document).on('change', 'input', function () {
+  var prev = $(this).data('val');
+  if(prev === ''){
+    prev = 0;
+  }else if(prev === 'x' || prev === 'X'){
+    prev = 10;
+  }else{
+    prev = parseInt(prev);
+  }
+
+  var cur = $(this).val();
+  if(cur === ''){
+    cur = 0;
+    // enable next input
+    const i = $('input').index($(this));
+    $('input').eq(i + 1).prop('disabled', false);
+
+  }else if(cur === 'x' || cur === 'X'){
+    cur = 10;
+
+    // enable the one after next input
+    const i = $('input').index($(this));
+    $('input').eq(i + 1).val('-');
+    $('input').eq(i + 2).prop('disabled', false);
+  }else{
+    cur = parseInt(cur);
+
+    // enable next input
+    const i = $('input').index($(this));
+    $('input').eq(i + 1).prop('disabled', false);
+  }
+
+  //calculate score
+  currentScore = currentScore - prev + cur;
+  $('.js-final-score-text').text(currentScore);
+  $('.js-final-score-val').val(currentScore);
+});
