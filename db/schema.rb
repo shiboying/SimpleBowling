d1dbcccb1a1ed16e7b198c969fa8e4c96ac3b2f3
boying.shi <shi.boying@gmail.com>
2017-12-12 22:52:54 -0800

    bs - create games table and its model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213065031) do
+ActiveRecord::Schema.define(version: 20171213065131) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "player_id"
+    t.string "one_first"
+    t.string "one_second"
+    t.string "two_first"
+    t.string "two_second"
+    t.string "three_first"
+    t.string "three_second"
+    t.string "four_first"
+    t.string "four_second"
+    t.string "five_first"
+    t.string "five_second"
+    t.string "six_first"
+    t.string "six_second"
+    t.string "seven_first"
+    t.string "seven_second"
+    t.string "eight_first"
+    t.string "eight_second"
+    t.string "nine_first"
+    t.string "nine_second"
+    t.string "ten_first"
+    t.string "ten_second"
+    t.string "ten_third"
+    t.string "final_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name", null: false
